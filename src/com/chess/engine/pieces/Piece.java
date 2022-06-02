7f089dfc1fe1fe95bad82dbdb18021129193815b
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-06-02 17:25:50 -0300

    Adicionado peça Knight - Parte 2

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -5,7 +5,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 
-import java.util.List;
+import java.util.Collection;
 
 public abstract class Piece {
 
@@ -19,6 +19,6 @@ public abstract class Piece {
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
     }
-    public abstract List<Move> calculateLegalMoves(final Board board);
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
 
 }
