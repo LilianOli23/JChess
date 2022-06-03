b4f5e94ce5c38050f3a56d713642ba3c37550d2b
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-06-02 22:56:16 -0300

    Introduzindo a classe Queen

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -11,12 +11,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+public class Queen extends Piece {
 
-public class Rook extends Piece {
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -9, -8, -7, -1, 1, 7, 8, 9 };
 
-    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -8, -1, 1, 8 };
-
-    Rook(int piecePosition, Alliance pieceAlliance) {
+    Queen(int piecePosition, Alliance pieceAlliance) {
 
         super(piecePosition, pieceAlliance);
     }
@@ -52,9 +51,9 @@ public class Rook extends Piece {
         return ImmutableList.copyOf(legalMoves);
     }
     private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1 );
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1 || candidateOffset == -9 || candidateOffset == 7);
     }
     private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == 1);
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 1 || candidateOffset == 9);
     }
 }
